7f76b8bb5af5cf01acc232f6926d66a825259470
Manuel Topete <manueltopt96@gmail.com>
2018-07-25 18:06:15 -0500

    prueba

--- SetupUserPS.ps1
+++ SetupUserPS.ps1
@@ -10,6 +10,10 @@
     return $option
 }
 
+#Prueba
+#Test
+#Probe
+
 Function Add-ComputerToDomain(){ 
    <# Param( 
       [string]$DomainAccountName = $(throw "Parameter missing: -DomainAccountName DomainAccountName"),  
